56638e23e0897ca37f4c306aa5bbce0f7405fe68
Churikova.AA <churikova.aa@dns-shop.ru>
2025-09-19 17:01:19 +1000

    docs: add docstring in Validator

--- validator.py
+++ validator.py
@@ -1,24 +1,25 @@
 class Validator:
-    """_summary_
+    """Валидатор входных строк CSV с транзакциями.
     """
     
     def __init__(self, supported_currencies: set[str]) -> None:
         """_summary_
 
         Args:
-            supported_currencies (set[str]): _description_
+            supported_currencies (set[str]): множество поддерживаемых валют 
         """
         
         self.supported_currencies = supported_currencies
 
     def validate(self, row: dict[str, str]) -> str | None:
-        """_summary_
+        """Проверяет строку транзакции на корректность.
 
         Args:
-            row (dict[str, str]): _description_
+            row (dict[str, str]):  одна строка CSV в виде словаря
 
         Returns:
-            str | None: _description_
+            str | None: сообщение об ошибке, если строка некорректна.
+                        None, если строка прошла валидацию.
         """
         
         if row["status"] != "completed":
